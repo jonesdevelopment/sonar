07f4c090753cefb845d95d66fae7236052179acf
jonesdevelopment <admin@jonesdev.xyz>
2023-06-29 21:50:51 +0200

    feat(bungee): add new and improved PacketCompressor

--- bungee/build.gradle.kts
+++ bungee/build.gradle.kts
@@ -1,5 +1,6 @@
 repositories {
   maven(url = "https://oss.sonatype.org/content/repositories/snapshots/") // BungeeCord
+  maven(url = "https://repo.papermc.io/repository/maven-public") // Velocity natives
 }
 
 dependencies {
@@ -7,6 +8,7 @@ dependencies {
   compileOnly(project(":sonar-common"))
 
   compileOnly("net.md-5:bungeecord-api:1.20-R0.1-SNAPSHOT")
+  implementation("com.velocitypowered:velocity-native:1.1.9")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
---
+++ bungee/src/main/java/jones/sonar/bungee/varint/VarIntUtil.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2023 jones
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package jones.sonar.bungee.varint
+
+import io.netty.buffer.ByteBuf
+import kotlin.math.ceil
+
+class VarIntUtil {
+  companion object {
+    private val EXACT_BYTE_LENGTHS = IntArray(33)
+
+    init {
+      for (i in 0..31) {
+        EXACT_BYTE_LENGTHS[i] = ceil((31.0 - (i - 1)) / 7).toInt()
+      }
+      EXACT_BYTE_LENGTHS[32] = 1; // Special case for the number 0.
+    }
+
+    fun varIntBytes(value: Int): Int {
+      return EXACT_BYTE_LENGTHS[Integer.numberOfLeadingZeros(value)]
+    }
+
+    fun write21BitVarInt(buf: ByteBuf, value: Int) {
+      // See https://steinborn.me/posts/performance/how-fast-can-you-write-a-varint/
+      val w = value and 0x7F or 0x80 shl 16 or (value ushr 7 and 0x7F or 0x80 shl 8) or (value ushr 14)
+      buf.writeMedium(w)
+    }
+
+    fun writeVarInt(buf: ByteBuf, value: Int) {
+      // Peel the one and two byte count cases explicitly as they are the most common VarInt sizes
+      // that the proxy will write, to improve inlining.
+      if (value and (-0x1 shl 7) == 0) {
+        buf.writeByte(value)
+      } else if (value and (-0x1 shl 14) == 0) {
+        val w = value and 0x7F or 0x80 shl 8 or (value ushr 7)
+        buf.writeShort(w)
+      } else {
+        writeVarIntFull(buf, value)
+      }
+    }
+
+    private fun writeVarIntFull(buf: ByteBuf, value: Int) {
+      // See https://steinborn.me/posts/performance/how-fast-can-you-write-a-varint/
+      if (value and (-0x1 shl 7) == 0) {
+        buf.writeByte(value)
+      } else if (value and (-0x1 shl 14) == 0) {
+        val w = value and 0x7F or 0x80 shl 8 or (value ushr 7)
+        buf.writeShort(w)
+      } else if (value and (-0x1 shl 21) == 0) {
+        val w = value and 0x7F or 0x80 shl 16 or (value ushr 7 and 0x7F or 0x80 shl 8) or (value ushr 14)
+        buf.writeMedium(w)
+      } else if (value and (-0x1 shl 28) == 0) {
+        val w = (value and 0x7F or 0x80 shl 24 or (value ushr 7 and 0x7F or 0x80 shl 16)
+          or (value ushr 14 and 0x7F or 0x80 shl 8) or (value ushr 21))
+        buf.writeInt(w)
+      } else {
+        val w = value and 0x7F or 0x80 shl 24 or (value ushr 7 and 0x7F or 0x80 shl 16
+          ) or (value ushr 14 and 0x7F or 0x80 shl 8) or (value ushr 21 and 0x7F or 0x80)
+        buf.writeInt(w)
+        buf.writeByte(value ushr 28)
+      }
+    }
+  }
+}
