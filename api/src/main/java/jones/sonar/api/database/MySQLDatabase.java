64a5f3b6a6c5e037d12aca7d1103083f48145ed4
jonesdevelopment <admin@jonesdev.xyz>
2023-07-17 18:10:16 +0200

    feat(db): improve MySQL integration

--- api/src/main/java/jones/sonar/api/database/MySQLDatabase.java
+++ api/src/main/java/jones/sonar/api/database/MySQLDatabase.java
@@ -24,12 +24,10 @@ import jones.sonar.api.config.SonarConfiguration;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.val;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -41,6 +39,7 @@ import java.util.Vector;
 public final class MySQLDatabase implements Database {
   @Getter
   private @Nullable HikariDataSource dataSource;
+  private Connection connection;
   public static final String VERIFIED_TABLE = "verified_ips";
   public static final String BLACKLIST_TABLE = "blacklisted_ips";
   public static final String IP_COLUMN = "ip_address";
@@ -48,39 +47,28 @@ public final class MySQLDatabase implements Database {
   @Override
   public void initialize(final @NotNull SonarConfiguration config) {
     try {
-      // TODO: fix this class loader so we don't have to implement JDBC and Hikari
-      final URL[] url = new URL[]{
-        new URL("https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/8.0.33/mysql-connector-j-8.0" +
-          ".33.jar"),
-        new URL("https://repo1.maven.org/maven2/com/zaxxer/HikariCP/4.0.3/HikariCP-4.0.3.jar")
-      };
+      // Register MySQL driver
+      Class.forName("com.mysql.cj.jdbc.Driver");
 
-      try (final URLClassLoader classLoader = new URLClassLoader(url, getClass().getClassLoader())) {
-        // Register MySQL driver
-        classLoader.loadClass("com.mysql.cj.jdbc.Driver");
+      // Set up hikari
+      final HikariConfig hikariConfig = new HikariConfig();
 
-        val configClass = (Class<HikariConfig>) classLoader.loadClass("com.zaxxer.hikari.HikariConfig");
-        // Create Hikari config
-        final HikariConfig hikariConfig = configClass.newInstance();
+      hikariConfig.setJdbcUrl(
+        "jdbc:mysql://" + config.DATABASE_URL + ":" + config.DATABASE_PORT + "/" + config.DATABASE_NAME
+      );
+      hikariConfig.setUsername(config.DATABASE_USERNAME);
+      hikariConfig.setPassword(config.DATABASE_PASSWORD);
 
-        hikariConfig.setJdbcUrl(
-          "jdbc:mysql://"
-            + config.DATABASE_URL
-            + ":" + config.DATABASE_PORT
-            + "/" + config.DATABASE_NAME
-        );
-        hikariConfig.setUsername(config.DATABASE_USERNAME);
-        hikariConfig.setPassword(config.DATABASE_PASSWORD);
+      // Connect to the server
+      dataSource = new HikariDataSource(hikariConfig);
+      connection = dataSource.getConnection();
 
-        // Connect to the server
-        dataSource = new HikariDataSource(hikariConfig);
-
-        // Create necessary tables and columns
-        createTable(IP_COLUMN, VERIFIED_TABLE);
-        createTable(IP_COLUMN, BLACKLIST_TABLE);
-      }
+      // Create necessary tables and columns
+      createTable(IP_COLUMN, VERIFIED_TABLE);
+      createTable(IP_COLUMN, BLACKLIST_TABLE);
     } catch (Throwable throwable) {
       Sonar.get().getLogger().error("Failed to connect to database: {}", throwable);
+      throwable.printStackTrace();
     }
   }
 
@@ -106,7 +94,7 @@ public final class MySQLDatabase implements Database {
 
     final Collection<String> output = new Vector<>();
 
-    try (final PreparedStatement statement = dataSource.getConnection().prepareStatement(
+    try (final PreparedStatement statement = connection.prepareStatement(
       "select `" + column + "` from `" + table + "` limit " + Sonar.get().getConfig().DATABASE_QUERY_LIMIT
     )) {
       final ResultSet resultSet = statement.executeQuery();
@@ -128,10 +116,10 @@ public final class MySQLDatabase implements Database {
     Objects.requireNonNull(dataSource);
 
     try (final PreparedStatement selectStatement =
-           dataSource.getConnection().prepareStatement("select 1 from `" + table + "` where `" + column +
+           connection.prepareStatement("select 1 from `" + table + "` where `" + column +
              "` = ?");
          final PreparedStatement insertStatement =
-           dataSource.getConnection().prepareStatement("insert into `" + table + "` (`" + column + "`) " +
+           connection.prepareStatement("insert into `" + table + "` (`" + column + "`) " +
              "values (?)")) {
       for (final String v : collection) {
         selectStatement.setString(1, v);
@@ -154,7 +142,7 @@ public final class MySQLDatabase implements Database {
   public void clear(final @NotNull String table) {
     Objects.requireNonNull(dataSource);
 
-    try (final PreparedStatement statement = dataSource.getConnection().prepareStatement(
+    try (final PreparedStatement statement = connection.prepareStatement(
       "delete from `" + table + "`"
     )) {
       statement.execute();
@@ -169,7 +157,7 @@ public final class MySQLDatabase implements Database {
     Objects.requireNonNull(dataSource);
 
     for (final String name : tables) {
-      try (final PreparedStatement statement = dataSource.getConnection().prepareStatement(
+      try (final PreparedStatement statement = connection.prepareStatement(
         "create table if not exists " + name + " (`" + column + "` varchar(16))"
       )) {
         statement.execute();
