49c8a72587087aeecab88956c6fbb97ecf7e10a6
jonesdevelopment <admin@jonesdev.xyz>
2023-07-25 00:15:07 +0200

    feat: add comments to every message

--- sonar-api/src/main/java/xyz/jonesdev/sonar/api/config/SonarConfiguration.java
+++ sonar-api/src/main/java/xyz/jonesdev/sonar/api/config/SonarConfiguration.java
@@ -136,7 +136,14 @@ public final class SonarConfiguration {
     messagesConfig.load();
 
     // Message settings
+    messagesConfig.getYaml().setComment("messages.prefix",
+      "Placeholder for every '%prefix%' in this configuration file"
+    );
     PREFIX = formatString(messagesConfig.getString("messages.prefix", "&e&lSonar &7» &f"));
+
+    messagesConfig.getYaml().setComment("messages.support-url",
+      "Placeholder for every '%support-url%' in this configuration file"
+    );
     SUPPORT_URL = messagesConfig.getString("messages.support-url", "https://jonesdev.xyz/discord/");
 
     // General options
@@ -293,29 +300,55 @@ public final class SonarConfiguration {
     ENABLE_COMPRESSION = generalConfig.getBoolean("general.verification.enable-compression", true);
 
     // load this here otherwise it could cause issues
+    messagesConfig.getYaml().setComment("messages.header",
+      "Placeholder for every '%header%' in this configuration file"
+    );
     HEADER = fromList(messagesConfig.getStringList("messages.header",
       Arrays.asList(
         "&e&lSonar"
       )));
+
+    messagesConfig.getYaml().setComment("messages.footer",
+      "Placeholder for every '%footer%' in this configuration file"
+    );
     FOOTER = fromList(messagesConfig.getStringList("messages.footer",
       Arrays.asList(
         "&7If you believe that this is an error, contact an administrator."
       )));
 
+    messagesConfig.getYaml().setComment("messages.lockdown.enabled",
+      "Message that is shown when a player enables server lockdown"
+    );
     LOCKDOWN_ACTIVATED = formatString(messagesConfig.getString("messages.lockdown.enabled",
       "%prefix%The server is now in lockdown mode."
     ));
+
+    messagesConfig.getYaml().setComment("messages.lockdown.disabled",
+      "Message that is shown when a player disables server lockdown"
+    );
     LOCKDOWN_DEACTIVATED = formatString(messagesConfig.getString("messages.lockdown.disabled",
       "%prefix%The server is no longer in lockdown mode."
     ));
+
+    messagesConfig.getYaml().setComment("messages.lockdown.notification",
+      "Message that is shown when an admin joins the server during lockdown"
+    );
     LOCKDOWN_NOTIFICATION = formatString(messagesConfig.getString("messages.lockdown.notification",
       "%prefix%&aHey, the server is currently in lockdown mode. If you want to disable the lockdown mode, " +
         "type " +
         "&f/sonar" +
         " lockdown&a."
     ));
+
+    messagesConfig.getYaml().setComment("messages.lockdown.console-log",
+      "Message that is shown to console when a normal player tries joining the server during lockdown"
+    );
     LOCKDOWN_CONSOLE_LOG = messagesConfig.getString("messages.lockdown.console-log",
       "%player% (%ip%, %protocol%) tried to join during lockdown mode."
+    );
+
+    messagesConfig.getYaml().setComment("messages.lockdown.disconnect-message",
+      "Message that is shown to a normal player when they try joining the server during lockdown"
     );
     LOCKDOWN_DISCONNECT = fromList(messagesConfig.getStringList("messages.lockdown.disconnect-message",
       Arrays.asList(
@@ -324,113 +357,238 @@ public final class SonarConfiguration {
         "%footer%"
       )));
 
+    messagesConfig.getYaml().setComment("messages.reload.start",
+      "Message that is shown when someone starts reloading Sonar"
+    );
     RELOADING = formatString(messagesConfig.getString("messages.reload.start",
       "%prefix%Reloading Sonar..."
     ));
+
+    messagesConfig.getYaml().setComment("messages.reload.finish",
+      "Message that is shown when Sonar has finished reloading"
+    );
     RELOADED = formatString(messagesConfig.getString("messages.reload.finish",
       "%prefix%&aSuccessfully reloaded &7(%taken%ms)"
     ));
 
+    messagesConfig.getYaml().setComment("messages.verbose.subscribed",
+      "Message that is shown when a player subscribes to Sonar verbose"
+    );
     VERBOSE_SUBSCRIBED = formatString(messagesConfig.getString("messages.verbose.subscribed",
       "%prefix%You are now viewing Sonar verbose."
     ));
+
+    messagesConfig.getYaml().setComment("messages.verbose.unsubscribed",
+      "Message that is shown when a player unsubscribes from Sonar verbose"
+    );
     VERBOSE_UNSUBSCRIBED = formatString(messagesConfig.getString("messages.verbose.unsubscribed",
       "%prefix%You are no longer viewing Sonar verbose."
     ));
+
+    messagesConfig.getYaml().setComment("messages.verbose.subscribed-other",
+      "Message that is shown when a player makes another player subscribe to Sonar verbose"
+    );
     VERBOSE_SUBSCRIBED_OTHER = formatString(messagesConfig.getString("messages.verbose.subscribed-other",
       "%prefix%%player% is now viewing Sonar verbose."
     ));
+
+    messagesConfig.getYaml().setComment("messages.verbose.unsubscribed-other",
+      "Message that is shown when a player makes another player unsubscribe from Sonar verbose"
+    );
     VERBOSE_UNSUBSCRIBED_OTHER = formatString(messagesConfig.getString("messages.verbose.unsubscribed-other",
       "%prefix%%player% is no longer viewing Sonar verbose."
     ));
 
+    messagesConfig.getYaml().setComment("messages.database.disallowed",
+      "Message that is shown when someone tries purging the database when it is disallowed"
+    );
     DATABASE_PURGE_DISALLOWED = formatString(messagesConfig.getString("messages.database.disallowed",
       "%prefix%&cPurging the database is currently disallowed. Therefore, your action has been cancelled."
     ));
+
+    messagesConfig.getYaml().setComment("messages.database.purge-confirm",
+      "Message that is shown when someone tries purging the database and has to confirm their action"
+    );
     DATABASE_PURGE_CONFIRM = formatString(messagesConfig.getString("messages.database.purge-confirm",
       "%prefix%&cPlease confirm that you want to delete all database entries by typing &7/sonar database " +
         "purge " +
         "confirm&c."
     ));
-    DATABASE_PURGE = formatString(messagesConfig.getString("messages.database.purge",
+
+    messagesConfig.getYaml().setComment("messages.database.purged",
+      "Message that is shown when the database was successfully purged"
+    );
+    DATABASE_PURGE = formatString(messagesConfig.getString("messages.database.purged",
       "%prefix%&aSuccessfully purged all database entries."
     ));
-    DATABASE_PURGE_ALREADY = formatString(messagesConfig.getString("messages.database.purging",
+
+    messagesConfig.getYaml().setComment("messages.database.already-purging",
+      "Message that is shown when the database is already being purged"
+    );
+    DATABASE_PURGE_ALREADY = formatString(messagesConfig.getString("messages.database.already-purging",
       "%prefix%&cThere is already a purge currently running."
     ));
+
+    messagesConfig.getYaml().setComment("messages.database.not-selected",
+      "Message that is shown when no database is configured"
+    );
     DATABASE_NOT_SELECTED = formatString(messagesConfig.getString("messages.database.not-selected",
       "%prefix%&cYou have not selected any data storage type."
     ));
+
+    messagesConfig.getYaml().setComment("messages.database.reload.start",
+      "Message that is shown when someone starts reloading the database"
+    );
     DATABASE_RELOADING = formatString(messagesConfig.getString("messages.database.reload.start",
       "%prefix%Reloading all databases..."
     ));
+
+    messagesConfig.getYaml().setComment("messages.database.reload.finish",
+      "Message that is shown when the database has finished reloading"
+    );
     DATABASE_RELOADED = formatString(messagesConfig.getString("messages.database.reload.finish",
       "%prefix%&aSuccessfully reloaded &7(%taken%ms)"
     ));
 
+    messagesConfig.getYaml().setComment("messages.incorrect-command-usage",
+      "Message that is shown when someone uses a command incorrectly"
+    );
     INCORRECT_COMMAND_USAGE = formatString(messagesConfig.getString("messages.incorrect-command-usage",
       "%prefix%&cUsage: /sonar %usage%"
     ));
+
+    messagesConfig.getYaml().setComment("messages.invalid-ip-address",
+      "Message that is shown when someone provides an invalid IP address (Invalid characters)"
+    );
     INCORRECT_IP_ADDRESS = formatString(messagesConfig.getString("messages.invalid-ip-address",
       "%prefix%The IP address you provided seems to be invalid."
     ));
+
+    messagesConfig.getYaml().setComment("messages.illegal-ip-address",
+      "Message that is shown when someone provides an illegal IP address (Local IP)"
+    );
     ILLEGAL_IP_ADDRESS = formatString(messagesConfig.getString("messages.illegal-ip-address",
       "%prefix%The IP address you provided seems to be either a local or loopback IP."
     ));
-    PLAYERS_ONLY = formatString(messagesConfig.getString("messages.players-only",
+
+    messagesConfig.getYaml().setComment("messages.player-only",
+      "Message that is shown when the console runs a command that is player-only"
+    );
+    PLAYERS_ONLY = formatString(messagesConfig.getString("messages.player-only",
       "%prefix%&cYou can only execute this command as a player."
     ));
+
+    messagesConfig.getYaml().setComment("messages.console-only",
+      "Message that is shown when a player runs a command that is console-only"
+    );
     CONSOLE_ONLY = formatString(messagesConfig.getString("messages.console-only",
       "%prefix%&cFor security reasons, you can only execute this command through console."
     ));
+
+    messagesConfig.getYaml().setComment("messages.command-cool-down",
+      "Message that is shown when a player executes Sonar commands too quickly"
+    );
     COMMAND_COOL_DOWN = formatString(messagesConfig.getString("messages.command-cool-down",
       "%prefix%&cYou can only execute this command every 0.5 seconds."
     ));
     COMMAND_COOL_DOWN_LEFT = formatString(messagesConfig.getString("messages.command-cool-down-left",
       "%prefix%&cPlease wait another &l%time-left%s&r&c."
     ));
+
+    messagesConfig.getYaml().setComment("messages.sub-command-no-permission",
+      "Message that is shown when a player does not have permission to execute a certain subcommand"
+    );
     SUB_COMMAND_NO_PERM = formatString(messagesConfig.getString("messages.sub-command-no-permission",
       "%prefix%&cYou do not have permission to execute this subcommand. &7(%permission%)"
     ));
 
+    messagesConfig.getYaml().setComment("messages.blacklist.empty",
+      "Message that is shown when someone tries clearing the blacklist but is is empty"
+    );
     BLACKLIST_EMPTY = formatString(messagesConfig.getString("messages.blacklist.empty",
       "%prefix%The blacklist is currently empty. Therefore, no IP addresses were removed from the blacklist."
     ));
+
+    messagesConfig.getYaml().setComment("messages.blacklist.cleared",
+      "Message that is shown when someone clears the blacklist"
+    );
     BLACKLIST_CLEARED = formatString(messagesConfig.getString("messages.blacklist.cleared",
       "%prefix%You successfully removed a total of %removed% IP address(es) from the blacklist."
     ));
+
+    messagesConfig.getYaml().setComment("messages.blacklist.size",
+      "Message that is shown when someone checks the size of the blacklist"
+    );
     BLACKLIST_SIZE = formatString(messagesConfig.getString("messages.blacklist.size",
       "%prefix%The blacklist currently contains %amount% IP address(es)."
     ));
+
+    messagesConfig.getYaml().setComment("messages.blacklist.added",
+      "Message that is shown when someone adds an IP address to the blacklist"
+    );
     BLACKLIST_ADD = formatString(messagesConfig.getString("messages.blacklist.added",
       "%prefix%Successfully added %ip% to the blacklist."
     ));
+
+    messagesConfig.getYaml().setComment("messages.blacklist.removed",
+      "Message that is shown when someone removes an IP address from the blacklist"
+    );
     BLACKLIST_REMOVE = formatString(messagesConfig.getString("messages.blacklist.removed",
       "%prefix%Successfully removed %ip% from the blacklist."
     ));
+
+    messagesConfig.getYaml().setComment("messages.blacklist.duplicate-ip",
+      "Message that is shown when someone adds an IP address to the blacklist but it is already blacklisted"
+    );
     BLACKLIST_DUPLICATE = formatString(messagesConfig.getString("messages.blacklist.duplicate-ip",
       "%prefix%The IP address you provided is already blacklisted."
     ));
+
+    messagesConfig.getYaml().setComment("messages.blacklist.ip-not-found",
+      "Message that is shown when someone removes an IP address from the blacklist but it is not blacklisted"
+    );
     BLACKLIST_NOT_FOUND = formatString(messagesConfig.getString("messages.blacklist.ip-not-found",
       "%prefix%The IP address you provided is not blacklisted."
     ));
 
+    messagesConfig.getYaml().setComment("messages.whitelist.size",
+      "Message that is shown when someone checks the size of the whitelist"
+    );
     WHITELIST_SIZE = formatString(messagesConfig.getString("messages.whitelist.size",
       "%prefix%The whitelist currently contains %amount% IP address(es)."
     ));
+
+    messagesConfig.getYaml().setComment("messages.whitelist.added",
+      "Message that is shown when someone adds an IP address to the whitelist"
+    );
     WHITELIST_ADD = formatString(messagesConfig.getString("messages.whitelist.added",
       "%prefix%Successfully added %ip% to the whitelist."
     ));
+
+    messagesConfig.getYaml().setComment("messages.whitelist.removed",
+      "Message that is shown when someone removes an IP address from the whitelist"
+    );
     WHITELIST_REMOVE = formatString(messagesConfig.getString("messages.whitelist.removed",
       "%prefix%Successfully removed %ip% from the whitelist."
     ));
+
+    messagesConfig.getYaml().setComment("messages.whitelist.duplicate-ip",
+      "Message that is shown when someone adds an IP address to the whitelist but it is already whitelisted"
+    );
     WHITELIST_DUPLICATE = formatString(messagesConfig.getString("messages.whitelist.duplicate-ip",
       "%prefix%The IP address you provided is already whitelisted."
     ));
+
+    messagesConfig.getYaml().setComment("messages.whitelist.ip-not-found",
+      "Message that is shown when someone removes an IP address from the whitelist but it is not whitelisted"
+    );
     WHITELIST_NOT_FOUND = formatString(messagesConfig.getString("messages.whitelist.ip-not-found",
       "%prefix%The IP address you provided is not whitelisted."
     ));
 
+    messagesConfig.getYaml().setComment("messages.verification.too-many-players",
+      "Disconnect message that is shown when too many players are verifying at the same time"
+    );
     TOO_MANY_PLAYERS = fromList(messagesConfig.getStringList("messages.verification.too-many-players",
       Arrays.asList(
         "%header%",
@@ -438,6 +596,10 @@ public final class SonarConfiguration {
         "&7Please wait a few seconds before trying to join again.",
         "%footer%"
       )));
+
+    messagesConfig.getYaml().setComment("messages.verification.too-fast-reconnect",
+      "Disconnect message that is shown when someone rejoins too fast during verification"
+    );
     TOO_FAST_RECONNECT = fromList(messagesConfig.getStringList("messages.verification.too-fast-reconnect",
       Arrays.asList(
         "%header%",
@@ -445,6 +607,10 @@ public final class SonarConfiguration {
         "&7Please wait a few seconds before trying to verify again.",
         "%footer%"
       )));
+
+    messagesConfig.getYaml().setComment("messages.verification.already-verifying",
+      "Disconnect message that is shown when someone joins but is already verifying"
+    );
     ALREADY_VERIFYING = fromList(messagesConfig.getStringList("messages.verification.already-verifying",
       Arrays.asList(
         "%header%",
@@ -452,6 +618,10 @@ public final class SonarConfiguration {
         "&cPlease wait a few seconds before trying to verify again.",
         "%footer%"
       )));
+
+    messagesConfig.getYaml().setComment("messages.verification.already-queued",
+      "Disconnect message that is shown when someone joins but is already queued for verification"
+    );
     ALREADY_QUEUED = fromList(messagesConfig.getStringList("messages.verification.already-queued",
       Arrays.asList(
         "%header%",
@@ -459,6 +629,10 @@ public final class SonarConfiguration {
         "&cPlease wait a few minutes before trying to verify again.",
         "%footer%"
       )));
+
+    messagesConfig.getYaml().setComment("messages.verification.blacklisted",
+      "Disconnect message that is shown when someone joins but is temporarily blacklisted"
+    );
     BLACKLISTED = fromList(messagesConfig.getStringList("messages.verification.blacklisted",
       Arrays.asList(
         "%header%",
@@ -467,6 +641,10 @@ public final class SonarConfiguration {
         "&6False positive? &7%support-url%",
         "%footer%"
       )));
+
+    messagesConfig.getYaml().setComment("messages.verification.unexpected-error",
+      "Disconnect message that is shown when someone tries verifying but something goes wrong (Usually lag)"
+    );
     UNEXPECTED_ERROR = fromList(messagesConfig.getStringList("messages.verification.unexpected-error",
       Arrays.asList(
         "%header%",
@@ -475,12 +653,20 @@ public final class SonarConfiguration {
         "&6Need help? &7%support-url%",
         "%footer%"
       )));
+
+    messagesConfig.getYaml().setComment("messages.verification.invalid-username",
+      "Disconnect message that is shown when someone joins with an invalid username"
+    );
     INVALID_USERNAME = fromList(messagesConfig.getStringList("messages.verification.invalid-username",
       Arrays.asList(
         "%header%",
         "&cYour username contains invalid characters.",
         "%footer%"
       )));
+
+    messagesConfig.getYaml().setComment("messages.verification.too-many-online-per-ip",
+      "Disconnect message that is shown when someone joins but there are too many online players with their IP address"
+    );
     TOO_MANY_ONLINE_PER_IP = fromList(messagesConfig.getStringList("messages.too-many-online-per-ip",
       Arrays.asList(
         "%header%",
@@ -488,6 +674,9 @@ public final class SonarConfiguration {
         "%footer%"
       )));
 
+    messagesConfig.getYaml().setComment("messages.action-bar.layout",
+      "General layout for the verbose action-bar"
+    );
     ACTION_BAR_LAYOUT = formatString(messagesConfig.getString(
       "messages.action-bar.layout",
       "%prefix%&fQueued &7%queued%" +
