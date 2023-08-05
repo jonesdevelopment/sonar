87c3add43a9f18b37b666255462e3e091b7d462c
jonesdevelopment <admin@jonesdev.xyz>
2023-08-05 11:44:21 +0200

    feat: add Varint21FrameDecoder and Varint21ByteDecoder

---
+++ sonar-bungee/src/main/java/xyz/jonesdev/sonar/bungee/fallback/varint/Varint21ByteDecoder.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2023 Sonar Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package xyz.jonesdev.sonar.bungee.fallback.varint;
+
+import io.netty.util.ByteProcessor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public final class Varint21ByteDecoder implements ByteProcessor {
+  private DecoderResult result = DecoderResult.TOO_SMALL;
+  private int readVarInt, bytesRead;
+
+  @Override
+  public boolean process(byte b) throws Exception {
+    if (b == 0 && bytesRead == 0) {
+      result = DecoderResult.RUN_OF_ZEROES;
+      return true;
+    }
+
+    if (result == DecoderResult.RUN_OF_ZEROES) {
+      return false;
+    }
+
+    readVarInt |= (b & 0x7F) << bytesRead++ * 7;
+
+    if (bytesRead > 3) {
+      result = DecoderResult.TOO_BIG;
+      return false;
+    }
+
+    if ((b & 0x80) != 128) {
+      result = DecoderResult.SUCCESS;
+      return false;
+    }
+    return true;
+  }
+
+  public enum DecoderResult {
+    SUCCESS,
+    TOO_SMALL,
+    TOO_BIG,
+    RUN_OF_ZEROES
+  }
+}
---
+++ sonar-bungee/src/main/java/xyz/jonesdev/sonar/bungee/fallback/varint/Varint21FrameDecoder.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2023 Sonar Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package xyz.jonesdev.sonar.bungee.fallback.varint;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.CorruptedFrameException;
+
+import java.util.List;
+
+public final class Varint21FrameDecoder extends ByteToMessageDecoder {
+
+  @Override
+  protected void decode(final ChannelHandlerContext ctx,
+                        final ByteBuf byteBuf,
+                        final List<Object> out) throws Exception {
+    if (!ctx.channel().isActive() || !byteBuf.isReadable()) {
+      // MessageToMessageDecoder always do release after decode
+      // https://github.com/jonesdevelopment/sonar/pull/5
+      return;
+    }
+    System.out.println(byteBuf.readableBytes());
+
+    final Varint21ByteDecoder reader = new Varint21ByteDecoder();
+    final int end = byteBuf.forEachByte(reader);
+
+    if (end == -1) {
+      // This is probably a good sign that the buffer was too short or empty
+      // since the ByteBuf cannot hold a proper VarInt.
+      if (reader.getResult() == Varint21ByteDecoder.DecoderResult.RUN_OF_ZEROES) {
+        byteBuf.clear();
+      }
+      return;
+    }
+
+    switch (reader.getResult()) {
+      case RUN_OF_ZEROES: {
+        // this will return to the point where the next varInt starts
+        byteBuf.readerIndex(end);
+        break;
+      }
+
+      case SUCCESS: {
+        final int readVarInt = reader.getReadVarInt();
+
+        if (readVarInt < 0) {
+          byteBuf.clear();
+          throw new CorruptedFrameException("Result: " + reader);
+        }
+
+        else if (readVarInt == 0) {
+          // Actually, we don't want to throw an Exception if the packet is empty.
+          // The check would also false flag a lot of legit players since packets
+          // in 1.7 could sometimes be empty.
+          byteBuf.readerIndex(end + 1);
+          return;
+        }
+
+        final int bytesRead = reader.getBytesRead();
+        final int minimumRead = bytesRead + readVarInt;
+
+        if (byteBuf.isReadable(minimumRead)) {
+          out.add(byteBuf.retainedSlice(end + 1, readVarInt));
+
+          byteBuf.skipBytes(minimumRead);
+        }
+        break;
+      }
+
+      default: {
+        byteBuf.clear();
+        throw new CorruptedFrameException("Result: " + reader);
+      }
+    }
+  }
+}
