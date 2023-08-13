7d8ce9e06b55e358f66975e95a2aed0888b9dbc4
jonesdevelopment <admin@jonesdev.xyz>
2023-08-13 18:44:43 +0200

    fix: the build script makes it so all gradle files can be executed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Fix permissions so we are actually able to run our gradle script
-chmod +x gradlew
+chmod +x gradle*
 # You can also use clean shadowJar if something goes wrong here
 # e.g. "./gradlew clean shadowJar --stacktrace"
 ./gradlew build-sonar --stacktrace
