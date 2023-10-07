cfa64755755e5f24828e3a16013d240382243e5b
Michel Elkenwaat <jonesdevelopment@proton.me>
2023-10-07 09:57:42 +0200

    feat: reimplement the dumping system using Gson

--- sonar-common/src/main/java/xyz/jonesdev/sonar/common/subcommand/impl/DumpCommand.java
+++ sonar-common/src/main/java/xyz/jonesdev/sonar/common/subcommand/impl/DumpCommand.java
@@ -17,38 +17,101 @@
 
 package xyz.jonesdev.sonar.common.subcommand.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+import xyz.jonesdev.sonar.api.SonarPlatform;
 import xyz.jonesdev.sonar.api.command.CommandInvocation;
 import xyz.jonesdev.sonar.api.command.subcommand.Subcommand;
 import xyz.jonesdev.sonar.api.command.subcommand.SubcommandInfo;
 import xyz.jonesdev.sonar.api.profiler.JVMProfiler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 // This command is only used to display helpful information in order to help
-// the Sonar developer(s) to fix issues more quickly
+// the Sonar contributors to fix issues more quickly
 @SubcommandInfo(
   name = "dump",
   description = "Print developer information",
   onlyConsole = true
 )
 public final class DumpCommand extends Subcommand implements JVMProfiler {
+  private static final Gson GSON = new GsonBuilder()
+    .disableInnerClassSerialization()
+    .create();
 
   @Override
   public void execute(final @NotNull CommandInvocation invocation) {
-    final String osName = System.getProperty("os.name");
-    final String osArch = System.getProperty("os.arch");
-    final String jvmArgs = String.join(",", RUNTIME_MX_BEAN.getInputArguments());
+    final String json = GSON.toJson(collectMappedInformation());
+    SONAR.getLogger().info("Generated dump: {}", json);
+  }
 
-    invocation.getSender().sendMessage("Virtual cores: " + getVirtualCores());
-    invocation.getSender().sendMessage("JVM args: " + jvmArgs);
-    invocation.getSender().sendMessage("Java name: " + RUNTIME_MX_BEAN.getVmName());
-    invocation.getSender().sendMessage("Java version: " + RUNTIME_MX_BEAN.getVmVersion());
-    invocation.getSender().sendMessage("Java vendor: " + RUNTIME_MX_BEAN.getVmVendor());
-    invocation.getSender().sendMessage("Operating system: " + osName + ", " + osArch);
-    invocation.getSender().sendMessage("Total memory: " + formatMemory(getTotalMemory()));
-    invocation.getSender().sendMessage("Max memory: " + formatMemory(getMaxMemory()));
-    invocation.getSender().sendMessage("Free memory: " + formatMemory(getFreeMemory()));
-    invocation.getSender().sendMessage("Used Memory: " + formatMemory(getUsedMemory()));
-    invocation.getSender().sendMessage("Platform: " + SONAR.getServer().getPlatform().getDisplayName());
-    invocation.getSender().sendMessage("Version: " + SONAR.getVersion().getFull());
+  @Unmodifiable
+  private @NotNull Map<String, Object> collectMappedInformation() {
+    val mappings = new WeakHashMap<String, Object>();
+    mappings.put("sonar", new Dump.Sonar(
+      SONAR.getVersion().getFull(),
+      SONAR.getServer().getPlatform(),
+      SONAR.getVersion().isOnMainBranch()
+    ));
+    mappings.put("runtime", new Dump.Runtime(
+      getVirtualCores(),
+      RUNTIME_MX_BEAN.getInputArguments(),
+      RUNTIME_MX_BEAN.getVmName(),
+      RUNTIME_MX_BEAN.getVmVendor(),
+      RUNTIME_MX_BEAN.getVmVersion()
+    ));
+    mappings.put("os", new Dump.OS(
+      System.getProperty("os.name"),
+      System.getProperty("os.arch"),
+      System.getProperty("os.version")
+    ));
+    mappings.put("memory", new Dump.Memory(
+      formatMemory(getTotalMemory()),
+      formatMemory(getMaxMemory()),
+      formatMemory(getFreeMemory()),
+      formatMemory(getUsedMemory())
+    ));
+    return mappings;
+  }
+
+  @SuppressWarnings("unused")
+  private static final class Dump {
+
+    @RequiredArgsConstructor
+    private static final class Sonar {
+      private final String version;
+      private final SonarPlatform platform;
+      private final boolean isOnMainBranch;
+    }
+
+    @RequiredArgsConstructor
+    private static final class Runtime {
+      private final int virtualCores;
+      private final List<String> jvmArguments;
+      private final String vmName;
+      private final String vmVendor;
+      private final String vmVersion;
+    }
+
+    @RequiredArgsConstructor
+    private static final class Memory {
+      private final String total;
+      private final String max;
+      private final String free;
+      private final String used;
+    }
+
+    @RequiredArgsConstructor
+    private static final class OS {
+      private final String name;
+      private final String arch;
+      private final String version;
+    }
   }
 }
