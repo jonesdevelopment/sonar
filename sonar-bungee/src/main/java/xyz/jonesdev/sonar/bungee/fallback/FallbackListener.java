860594400a92960acafc255523398123fc6ab6e2
Jones <73846784+jonesdevelopment@users.noreply.github.com>
2023-10-28 01:20:25 +0200

    Implement max-online-per-ip check on BungeeCord (#81)
    * feat(bungee): implement missing max online per ip check
    
    * fix(bungee): use correct component serialization for the lockdown notification message
    
    * fix(bungee): use correct disconnect message for max online per ip check
    
    * fix(bungee): add 1 to current online per ip count because the player joining isn't counted
    
    * fix(bungee): use adventure audience for sending the message

--- sonar-bungee/src/main/java/xyz/jonesdev/sonar/bungee/fallback/FallbackListener.java
+++ sonar-bungee/src/main/java/xyz/jonesdev/sonar/bungee/fallback/FallbackListener.java
@@ -18,19 +18,50 @@
 package xyz.jonesdev.sonar.bungee.fallback;
 
 import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.PendingConnection;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.protocol.packet.Kick;
 import org.jetbrains.annotations.NotNull;
 import xyz.jonesdev.sonar.api.Sonar;
+import xyz.jonesdev.sonar.bungee.SonarBungee;
+
+import java.net.InetAddress;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public final class FallbackListener implements Listener {
+
+  @EventHandler
+  @SuppressWarnings("deprecation")
+  public void handle(final @NotNull LoginEvent event) {
+    final InetAddress inetAddress = event.getConnection().getAddress().getAddress();
+
+    // Check if the number of online players using the same IP address as
+    // the connecting player is greater than the configured amount
+    final int maxOnlinePerIp = Sonar.get().getConfig().getMaxOnlinePerIp();
+
+    if (maxOnlinePerIp > 0) {
+      final long onlinePerIp = SonarBungee.INSTANCE.getPlugin().getServer().getPlayers().stream()
+        .filter(player -> Objects.equals(player.getAddress().getAddress(), inetAddress))
+        .count()
+        + 1 /* add 1 because the player hasn't been added to the list of online players yet */;
+
+      // We use '>=' because the player connecting to the server hasn't joined yet
+      if (onlinePerIp >= maxOnlinePerIp) {
+        final FallbackInitialHandler fallbackInitialHandler = (FallbackInitialHandler) event.getConnection();
+        final Component component = Sonar.get().getConfig().getTooManyOnlinePerIp();
+        final String serialized = JSONComponentSerializer.json().serialize(component);
+        fallbackInitialHandler.closeWith(new Kick(serialized));
+      }
+    }
+  }
 
   @EventHandler
   @SuppressWarnings("deprecation")
@@ -60,7 +91,12 @@ public final class FallbackListener implements Listener {
               String.valueOf(event.getPlayer().getPendingConnection().getVersion())));
         }
       } else if (Sonar.get().getConfig().getLockdown().isNotifyAdmins()) {
-        event.getPlayer().sendMessage(new TextComponent(Sonar.get().getConfig().getLockdown().getNotification()));
+        final Audience audience = Sonar.get().getVerboseHandler().getAudiences().get(event.getPlayer().getName());
+        if (audience != null) {
+          final String notification = Sonar.get().getConfig().getLockdown().getNotification();
+          final Component deserialized = MiniMessage.miniMessage().deserialize(notification);
+          audience.sendMessage(deserialized);
+        }
       }
     }
   }
