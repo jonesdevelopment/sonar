20f5b135aa8334cfac597e88d4e301fc8c2446e2
Jones <73846784+jonesdevelopment@users.noreply.github.com>
2023-10-28 01:04:01 +0200

    Fix lockdown notification not working as intended (#80)
    * fix: lockdown notification kicking administrators
    
    * fix: lockdown notification not being deserialized properly

--- sonar-velocity/src/main/java/xyz/jonesdev/sonar/velocity/fallback/FallbackListener.java
+++ sonar-velocity/src/main/java/xyz/jonesdev/sonar/velocity/fallback/FallbackListener.java
@@ -20,6 +20,7 @@ package xyz.jonesdev.sonar.velocity.fallback;
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.LoginEvent;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.proxy.crypto.IdentifiedKey;
 import com.velocitypowered.api.util.GameProfile;
@@ -36,6 +37,7 @@ import io.netty.channel.ChannelPipeline;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.jetbrains.annotations.NotNull;
 import xyz.jonesdev.sonar.api.ReflectiveOperationException;
 import xyz.jonesdev.sonar.api.Sonar;
@@ -349,23 +351,20 @@ public final class FallbackListener {
   public void handle(final @NotNull LoginEvent event) {
     val connectedPlayer = (ConnectedPlayer) event.getPlayer();
 
-    if (Sonar.get().getConfig().getLockdown().isEnabled()) {
-      if (!event.getPlayer().hasPermission(Sonar.get().getConfig().getLockdown().getBypassPermission())) {
-        connectedPlayer.getConnection().closeWith(Disconnect.create(
-          Sonar.get().getConfig().getLockdown().getDisconnect(), connectedPlayer.getProtocolVersion()));
+    if (Sonar.get().getConfig().getLockdown().isEnabled()
+      && !event.getPlayer().hasPermission(Sonar.get().getConfig().getLockdown().getBypassPermission())) {
+      connectedPlayer.getConnection().closeWith(Disconnect.create(
+        Sonar.get().getConfig().getLockdown().getDisconnect(), connectedPlayer.getProtocolVersion()));
 
-        if (Sonar.get().getConfig().getLockdown().isLogAttempts()) {
-          Sonar.get().getLogger().info(Sonar.get().getConfig().getLockdown().getConsoleLog()
-            .replace("%player%", event.getPlayer().getUsername())
-            .replace("%ip%", Sonar.get().getConfig()
-              .formatAddress(event.getPlayer().getRemoteAddress().getAddress()))
-            .replace("%protocol%",
-              String.valueOf(event.getPlayer().getProtocolVersion().getProtocol())));
-        }
-        return;
-      } else if (Sonar.get().getConfig().getLockdown().isNotifyAdmins()) {
-        event.getPlayer().sendMessage(Component.text(Sonar.get().getConfig().getLockdown().getNotification()));
+      if (Sonar.get().getConfig().getLockdown().isLogAttempts()) {
+        Sonar.get().getLogger().info(Sonar.get().getConfig().getLockdown().getConsoleLog()
+          .replace("%player%", event.getPlayer().getUsername())
+          .replace("%ip%", Sonar.get().getConfig()
+            .formatAddress(event.getPlayer().getRemoteAddress().getAddress()))
+          .replace("%protocol%",
+            String.valueOf(event.getPlayer().getProtocolVersion().getProtocol())));
       }
+      return;
     }
 
     final InetAddress inetAddress = event.getPlayer().getRemoteAddress().getAddress();
@@ -384,6 +383,17 @@ public final class FallbackListener {
         connectedPlayer.getConnection().closeWith(Disconnect.create(
           Sonar.get().getConfig().getTooManyOnlinePerIp(), connectedPlayer.getProtocolVersion()
         ));
+      }
+    }
+  }
+
+  @Subscribe(order = PostOrder.LAST)
+  public void handle(final @NotNull PostLoginEvent event) {
+    if (Sonar.get().getConfig().getLockdown().isEnabled() && Sonar.get().getConfig().getLockdown().isNotifyAdmins()) {
+      if (event.getPlayer().hasPermission(Sonar.get().getConfig().getLockdown().getBypassPermission())) {
+        final String notification = Sonar.get().getConfig().getLockdown().getNotification();
+        final Component deserialized = MiniMessage.miniMessage().deserialize(notification);
+        event.getPlayer().sendMessage(deserialized);
       }
     }
   }
