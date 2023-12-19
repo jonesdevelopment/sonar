237de5f27e874b436fed1a92d19a0abe40a8c3d6
Michel Elkenwaat <admin@jonesdev.xyz>
2023-12-19 16:34:10 +0100

    fix(bukkit): use highest event priority for audience listener

--- sonar-bukkit/src/main/java/xyz/jonesdev/sonar/bukkit/audience/AudienceListener.java
+++ sonar-bukkit/src/main/java/xyz/jonesdev/sonar/bukkit/audience/AudienceListener.java
@@ -29,13 +29,13 @@ import xyz.jonesdev.sonar.bukkit.SonarBukkit;
 
 public final class AudienceListener implements Listener {
 
-  @EventHandler(priority = EventPriority.LOWEST)
+  @EventHandler(priority = EventPriority.HIGHEST)
   public void handle(final @NotNull PlayerJoinEvent event) {
     final Audience audience = SonarBukkit.INSTANCE.getBukkitAudiences().player(event.getPlayer());
     Sonar.get().getVerboseHandler().getAudiences().put(event.getPlayer().getName(), audience);
   }
 
-  @EventHandler(priority = EventPriority.LOWEST)
+  @EventHandler(priority = EventPriority.HIGHEST)
   public void handle(final @NotNull PlayerQuitEvent event) {
     Sonar.get().getVerboseHandler().getAudiences().remove(event.getPlayer().getName());
   }
