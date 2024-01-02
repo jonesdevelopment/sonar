1e18a29d1ea3f089d75adadcace8e7fc4b4f6760
Jones <73846784+jonesdevelopment@users.noreply.github.com>
2024-01-02 18:17:20 +0100

    Update license year to 2023-2024 (#146)

--- sonar-common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/packets/play/Chat.java
+++ sonar-common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/packets/play/Chat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Sonar Contributors
+ * Copyright (C) 2023-2024 Sonar Contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
