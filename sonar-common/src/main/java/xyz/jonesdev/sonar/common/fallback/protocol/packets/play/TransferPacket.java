029f7920d0b3a0a415d56cabfa92b63e553ff26d
Michel Elkenwaat <admin@jonesdev.xyz>
2024-08-13 20:41:03 +0200

    Update copyright header

--- sonar-common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/packets/play/TransferPacket.java
+++ sonar-common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/packets/play/TransferPacket.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 jones
+ * Copyright (C) 2024 Sonar Contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
