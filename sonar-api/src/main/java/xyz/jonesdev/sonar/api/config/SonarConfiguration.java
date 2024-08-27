9b11c4c1e073fd92660902a8c7a78eb743f77c6f
Jones <admin@jonesdev.xyz>
2024-08-27 17:30:16 +0200

    Bump mariadb-java-client from 3.4.0 to 3.4.1

--- sonar-api/src/main/java/xyz/jonesdev/sonar/api/config/SonarConfiguration.java
+++ sonar-api/src/main/java/xyz/jonesdev/sonar/api/config/SonarConfiguration.java
@@ -498,7 +498,7 @@ public final class SonarConfiguration {
         Library.builder()
           .groupId("org{}mariadb{}jdbc")
           .artifactId("mariadb-java-client")
-          .version("3.4.0")
+          .version("3.4.1")
           .relocate("org{}mariadb", "xyz{}jonesdev{}sonar{}libs{}mariadb")
           .build()),
       H2("H2", "jdbc:h2:file:%s", new H2DatabaseTypeAdapter(),
