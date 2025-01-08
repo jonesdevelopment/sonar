c411cd5e39bdd451ecfc69bdaa10a2adc78dbc81
Jones <admin@jonesdev.xyz>
2025-01-08 18:23:11 +0100

    Fix codec nbt loading error

--- common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/dimension/DimensionRegistry.java
+++ common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/dimension/DimensionRegistry.java
@@ -54,7 +54,7 @@ public final class DimensionRegistry {
 
   private @NotNull CompoundBinaryTag getCodec(final @NotNull String fileName) {
     try (final InputStream inputStream = Sonar.class.getResourceAsStream("/assets/codecs/" + fileName)) {
-      return BinaryTagIO.reader().read(Objects.requireNonNull(inputStream), BinaryTagIO.Compression.GZIP);
+      return BinaryTagIO.unlimitedReader().read(Objects.requireNonNull(inputStream), BinaryTagIO.Compression.GZIP);
     } catch (Throwable throwable) {
       Sonar.get().getLogger().error("Could not load mappings for {}: {}", fileName, throwable);
       throw new IllegalStateException(throwable);
