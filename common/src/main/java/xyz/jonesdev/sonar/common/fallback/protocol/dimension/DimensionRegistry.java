179ca1ebe807c48061add3cb667a90b35e4855ec
Akkariin <fallencrystal@outlook.com>
2025-01-09 23:31:46 +0800

    Registries fixes & Fixed some unexpected behavior about mods. (#430)
    * Fix 1.20.5 + viafabricplus
    
    * Update registries

--- common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/dimension/DimensionRegistry.java
+++ common/src/main/java/xyz/jonesdev/sonar/common/fallback/protocol/dimension/DimensionRegistry.java
@@ -35,6 +35,7 @@ public final class DimensionRegistry {
   public final CompoundBinaryTag CODEC_1_19_1;
   public final CompoundBinaryTag CODEC_1_19_4;
   public final CompoundBinaryTag CODEC_1_20;
+  public final CompoundBinaryTag CODEC_1_20_5;
   public final CompoundBinaryTag CODEC_1_21;
   public final CompoundBinaryTag CODEC_1_21_2;
   public final CompoundBinaryTag CODEC_1_21_4;
@@ -47,6 +48,7 @@ public final class DimensionRegistry {
     CODEC_1_19_1 = getCodec("codec_1_19_1.nbt");
     CODEC_1_19_4 = getCodec("codec_1_19_4.nbt");
     CODEC_1_20 = getCodec("codec_1_20.nbt");
+    CODEC_1_20_5 = getCodec("codec_1_20_5.nbt");
     CODEC_1_21 = getCodec("codec_1_21.nbt");
     CODEC_1_21_2 = getCodec("codec_1_21_2.nbt");
     CODEC_1_21_4 = getCodec("codec_1_21_4.nbt");
