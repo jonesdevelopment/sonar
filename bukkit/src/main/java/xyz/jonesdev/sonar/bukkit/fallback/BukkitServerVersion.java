009dd08cb46911a4f40e69ac97432d85bc43cb02
Jones <admin@jonesdev.xyz>
2025-06-30 17:15:53 +0200

    Add missing server versions for 1.21.5-1.21.6

--- bukkit/src/main/java/xyz/jonesdev/sonar/bukkit/fallback/BukkitServerVersion.java
+++ bukkit/src/main/java/xyz/jonesdev/sonar/bukkit/fallback/BukkitServerVersion.java
@@ -74,7 +74,9 @@ public enum BukkitServerVersion {
   MINECRAFT_1_21_1,
   MINECRAFT_1_21_2,
   MINECRAFT_1_21_3,
-  MINECRAFT_1_21_4;
+  MINECRAFT_1_21_4,
+  MINECRAFT_1_21_5,
+  MINECRAFT_1_21_6;
 
   static final BukkitServerVersion[] REVERSED_VALUES;
 
