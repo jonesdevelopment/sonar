5e233b4f73a3eefba329d98557f9a0491a716eea
Jones <admin@jonesdev.xyz>
2025-08-31 15:43:57 +0200

    Revert "Refactor vehicle check (#497)"
    This reverts commit e7f816fcaa68ed603d2b804e08f4c018ba72159d.

--- common/src/main/java/xyz/jonesdev/sonar/common/fallback/verification/FallbackVehicleHandler.java
+++ common/src/main/java/xyz/jonesdev/sonar/common/fallback/verification/FallbackVehicleHandler.java
@@ -19,7 +19,6 @@ package xyz.jonesdev.sonar.common.fallback.verification;
 
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import xyz.jonesdev.sonar.api.Sonar;
 import xyz.jonesdev.sonar.api.fallback.FallbackUser;
 import xyz.jonesdev.sonar.api.fallback.protocol.ProtocolVersion;
@@ -41,9 +40,8 @@ public final class FallbackVehicleHandler extends FallbackVerificationHandler {
   private State state = State.WAITING;
   private State nextState;
   private int expectedKeepAliveId;
-  private int checkedRound;
+  private int rotations, inputs, paddles, vehicleMoves;
   private double boatMotion, boatY = IN_AIR_Y_POSITION;
-  private @Nullable ExpectVehiclePacket expectVehiclePacket = null;
 
   @RequiredArgsConstructor
   private enum State {
@@ -56,53 +54,74 @@ public final class FallbackVehicleHandler extends FallbackVerificationHandler {
     private final boolean inVehicle;
   }
 
-  private enum ExpectVehiclePacket {
-    PADDLE_BOAT,
-    PLAYER_ROTATION,
-    PLAYER_INPUT,
-    VEHICLE_MOVE,
-    CLIENT_TICK_END;
-
-    public @NotNull String formattedName() {
-      return name().toLowerCase().replace('_', ' ');
-    }
-  }
-
   @Override
   public void handle(final @NotNull FallbackPacket packet) {
     if (packet instanceof KeepAlivePacket) {
-      onKeepAlive((KeepAlivePacket) packet);
-    } else if (packet instanceof PaddleBoatPacket) {
-      onPaddleBoat((PaddleBoatPacket) packet);
-    } else if (packet instanceof VehicleMovePacket) {
-      onVehicleMove((VehicleMovePacket) packet);
-    } else if (packet instanceof SetPlayerRotationPacket) {
-      onPlayerRotation((SetPlayerRotationPacket) packet);
-    } else if (packet instanceof PlayerInputPacket) {
-      onPlayerInput((PlayerInputPacket) packet);
-    } else if (packet instanceof ClientTickEndPacket) {
-      onClientTickEnd((ClientTickEndPacket) packet);
-    } else if (packet instanceof SetPlayerPositionRotationPacket) {
-      final SetPlayerPositionRotationPacket posRot = (SetPlayerPositionRotationPacket) packet;
-      handleMovement(posRot.getY(), posRot.isOnGround());
-    } else if (packet instanceof SetPlayerPositionPacket) {
-      final SetPlayerPositionPacket position = (SetPlayerPositionPacket) packet;
-      handleMovement(position.getY(), position.isOnGround());
-    }
-  }
+      final KeepAlivePacket keepAlivePacket = (KeepAlivePacket) packet;
 
-  private void onKeepAlive(final @NotNull KeepAlivePacket keepAlive) {
-    // Check if we are expecting a KeepAlive packet
-    checkState(nextState != null, "invalid packet timing");
-    // Also check if the KeepAlive ID matches the expected ID
-    checkState(keepAlive.getId() == expectedKeepAliveId,
-      "expected K ID " + expectedKeepAliveId + ", but got " + keepAlive.getId());
+      // Check if we are expecting a KeepAlive packet
+      checkState(nextState != null, "invalid packet timing");
+      // Also check if the KeepAlive ID matches the expected ID
+      checkState(keepAlivePacket.getId() == expectedKeepAliveId,
+        "expected K ID " + expectedKeepAliveId + ", but got " + keepAlivePacket.getId());
 
-    state = nextState;
-    nextState = null;
-    waitingForStateChange = false;
-    if (!state.inVehicle) {
-      expectVehiclePacket = null;
+      state = nextState;
+      nextState = null;
+      waitingForStateChange = false;
+    } else if (!waitingForStateChange) {
+      if (packet instanceof PaddleBoatPacket) {
+        if (state == State.IN_BOAT) {
+          paddles++;
+        }
+      } else if (packet instanceof VehicleMovePacket) {
+        if (state == State.IN_BOAT) {
+          final VehicleMovePacket vehicleMove = (VehicleMovePacket) packet;
+          // Check the Y position of the vehicle
+          checkState(vehicleMove.getY() <= IN_AIR_Y_POSITION, "bad vehicle y: " + vehicleMove.getY());
+
+          // Check the gravity of the vehicle
+          final double lastBoatMotion = boatMotion;
+          final double lastBoatY = boatY;
+          boatY = vehicleMove.getY();
+          boatMotion = boatY - lastBoatY;
+          final double predicted = lastBoatMotion - 0.03999999910593033D;
+          final double difference = Math.abs(boatMotion - predicted);
+          // Check if the difference between the predicted and actual motion is too large
+          checkState(difference < 1e-7, "bad vehicle gravity: " + predicted + "/" + boatMotion);
+
+          vehicleMoves++;
+        }
+      } else if (packet instanceof SetPlayerRotationPacket) {
+        if (state.inVehicle) {
+          rotations++;
+
+          // 1.21.2+ do not send PlayerInput packets when inside a vehicle.
+          // Handle it after SetPlayerRotationPacket to simulate vanilla behavior.
+          if (user.getProtocolVersion().greaterThanOrEquals(ProtocolVersion.MINECRAFT_1_21_2)) {
+            handlePlayerInput();
+          }
+        }
+      } else if (packet instanceof PlayerInputPacket) {
+        // 1.21.2+ send PlayerInput packets when the player starts sprinting, sneaking, etc.
+        if (state.inVehicle && user.getProtocolVersion().lessThan(ProtocolVersion.MINECRAFT_1_21_2)) {
+          final PlayerInputPacket playerInput = (PlayerInputPacket) packet;
+
+          // Check if the player is sending invalid vehicle speed values
+          final float forward = Math.abs(playerInput.getForward());
+          final float sideways = Math.abs(playerInput.getSideways());
+          final float maxVehicleSpeed = /*user.isGeyser() ? 1 :*/ 0.98f;
+          checkState(forward <= maxVehicleSpeed, "illegal speed (f): " + forward);
+          checkState(sideways <= maxVehicleSpeed, "illegal speed (s): " + sideways);
+
+          handlePlayerInput();
+        }
+      } else if (packet instanceof SetPlayerPositionRotationPacket) {
+        final SetPlayerPositionRotationPacket posRot = (SetPlayerPositionRotationPacket) packet;
+        handleMovement(posRot.getY(), posRot.isOnGround());
+      } else if (packet instanceof SetPlayerPositionPacket) {
+        final SetPlayerPositionPacket position = (SetPlayerPositionPacket) packet;
+        handleMovement(position.getY(), position.isOnGround());
+      }
     }
   }
 
@@ -110,14 +129,13 @@ public final class FallbackVehicleHandler extends FallbackVerificationHandler {
     user.delayedWrite(nextState == State.IN_BOAT ? SPAWN_BOAT_ENTITY : SPAWN_MINECART_ENTITY);
     user.delayedWrite(SET_VEHICLE_PASSENGERS);
     prepareForNextState(nextState);
-    expectVehiclePacket = isClientMovingVehicle() ? ExpectVehiclePacket.PADDLE_BOAT : ExpectVehiclePacket.PLAYER_ROTATION;
   }
 
   private void prepareForNextState(final @NotNull State nextState) {
     this.nextState = nextState;
     waitingForStateChange = true;
     expectedKeepAliveId = RANDOM.nextInt();
-    checkedRound = 0;
+    rotations = inputs = paddles = vehicleMoves = 0;
     user.delayedWrite(new KeepAlivePacket(expectedKeepAliveId));
     user.channel().flush();
   }
@@ -133,118 +151,46 @@ public final class FallbackVehicleHandler extends FallbackVerificationHandler {
 
   private void handleMovement(final double y, final boolean isOnGround) {
     // Make sure we're currently expecting movement
-    if (state == State.WAITING) {
+    if (state.inVehicle || state == State.WAITING) {
       return;
     }
-    if (!state.inVehicle && !waitingForStateChange) {
-      expectVehiclePacket = null;
 
-      // Make sure the ground state and y position are correct
-      checkState(y <= boatY, "invalid y: " + y);
-      checkState(!isOnGround, "invalid ground state: " + y);
+    // Make sure the ground state and y position are correct
+    checkState(y <= boatY, "invalid y: " + y);
+    checkState(!isOnGround, "invalid ground state: " + y);
 
-      if (state == State.IN_AIR_AFTER_BOAT) {
-        spawnVehicle(State.IN_MINECART);
-      } else {
-        markSuccess();
-      }
-    }
-  }
-
-  private void onPaddleBoat(final @NotNull PaddleBoatPacket packet) {
-    checkState(isClientMovingVehicle(), "received unexpected paddle boat");
-    checkPacketOrder(ExpectVehiclePacket.PADDLE_BOAT);
-    expectVehiclePacket = ExpectVehiclePacket.PLAYER_ROTATION;
-  }
-
-  private void onPlayerRotation(final @NotNull SetPlayerRotationPacket packet) {
-    checkPacketOrder(ExpectVehiclePacket.PLAYER_ROTATION);
-
-    // Check yaw/pitch?
-
-    if (user.getProtocolVersion().lessThan(ProtocolVersion.MINECRAFT_1_21_2)) {
-      expectVehiclePacket = ExpectVehiclePacket.PLAYER_INPUT;
-    } else if (isClientMovingVehicle()) {
-      expectVehiclePacket = ExpectVehiclePacket.VEHICLE_MOVE;
+    if (state == State.IN_AIR_AFTER_BOAT) {
+      spawnVehicle(State.IN_MINECART);
     } else {
-      expectVehiclePacket = ExpectVehiclePacket.CLIENT_TICK_END;
+      markSuccess();
     }
   }
 
-  private void onPlayerInput(final @NotNull PlayerInputPacket packet) {
-    // 1.21.2+ send PlayerInput packets when the player starts sprinting, sneaking, etc.
-    if (user.getProtocolVersion().greaterThanOrEquals(ProtocolVersion.MINECRAFT_1_21_2)) {
-      return;
+  private void handlePlayerInput() {
+    // 1.8 and below do not have PaddleBoat packets, so we simply exempt them from the PaddleBoat check.
+    // Clients also don't send PaddleBoat & VehicleMovePacket packets while riding minecarts.
+    if (user.getProtocolVersion().lessThan(ProtocolVersion.MINECRAFT_1_9) || state == State.IN_MINECART) {
+      paddles++;
+      vehicleMoves++;
     }
-    checkPacketOrder(ExpectVehiclePacket.PLAYER_INPUT);
 
-    // Check if the player is sending invalid vehicle speed values
-    final float forward = Math.abs(packet.getForward());
-    final float sideways = Math.abs(packet.getSideways());
-    final float maxVehicleSpeed = /*user.isGeyser() ? 1 :*/ 0.98f;
-    checkState(forward <= maxVehicleSpeed, "illegal speed (f): " + forward);
-    checkState(sideways <= maxVehicleSpeed, "illegal speed (s): " + sideways);
+    // Check for packet order
+    checkState(rotations >= inputs,
+      "illegal packet order; i/r " + inputs + "/" + rotations);
+    checkState(paddles >= inputs,
+      "illegal packet order; i/p " + inputs + "/" + paddles);
+    checkState(vehicleMoves >= inputs,
+      "illegal packet order; i/v " + inputs + "/" + vehicleMoves);
 
-    if (isClientMovingVehicle()) {
-      expectVehiclePacket = ExpectVehiclePacket.VEHICLE_MOVE;
-    } else {
-      finishRound();
-    }
-  }
+    inputs++;
 
-  private void onVehicleMove(final @NotNull VehicleMovePacket packet) {
-    checkState(isClientMovingVehicle(), "received unexpected vehicle move");
-    checkPacketOrder(ExpectVehiclePacket.VEHICLE_MOVE);
-
-    // Check the Y position of the vehicle
-    checkState(packet.getY() <= IN_AIR_Y_POSITION, "bad vehicle y: " + packet.getY());
-
-    // Check the gravity of the vehicle
-    final double lastBoatMotion = boatMotion;
-    final double lastBoatY = boatY;
-    boatY = packet.getY();
-    boatMotion = boatY - lastBoatY;
-    final double predicted = lastBoatMotion - 0.03999999910593033D;
-    final double difference = Math.abs(boatMotion - predicted);
-    // Check if the difference between the predicted and actual motion is too large
-    checkState(difference < 1e-7, "bad vehicle gravity: " + predicted + "/" + boatMotion);
-
-    if (user.getProtocolVersion().lessThan(ProtocolVersion.MINECRAFT_1_21_2)) {
-      this.finishRound();
-    } else {
-      expectVehiclePacket = ExpectVehiclePacket.CLIENT_TICK_END;
-    }
-  }
-
-  private void onClientTickEnd(final @NotNull ClientTickEndPacket packet) {
-    if (expectVehiclePacket == ExpectVehiclePacket.CLIENT_TICK_END) {
-      finishRound();
-    } else if (!waitingForStateChange && state.inVehicle) {
-      // Client can send end tick while not in vehicle
-      checkPacketOrder(ExpectVehiclePacket.CLIENT_TICK_END);
-    }
-  }
-
-  private void finishRound() {
+    // Check if we've received more than the minimum number of packets
     final int minimumPackets = Sonar.get0().getConfig().getVerification().getVehicle().getMinimumPackets();
-    if (++checkedRound > minimumPackets) {
+    if (inputs > minimumPackets && rotations > minimumPackets
+      && paddles > minimumPackets && vehicleMoves > minimumPackets) {
       // Move on to the next stage
-      expectVehiclePacket = null;
       user.delayedWrite(REMOVE_VEHICLE);
       prepareForNextState(state == State.IN_BOAT ? State.IN_AIR_AFTER_BOAT : State.IN_AIR_AFTER_MINECART);
-    } else {
-      expectVehiclePacket = isClientMovingVehicle() ? ExpectVehiclePacket.PADDLE_BOAT : ExpectVehiclePacket.PLAYER_ROTATION;
     }
-  }
-
-  private boolean isClientMovingVehicle() {
-    return (waitingForStateChange && nextState.inVehicle ? nextState : state) == State.IN_BOAT
-      && user.getProtocolVersion().greaterThan(ProtocolVersion.MINECRAFT_1_8);
-  }
-
-  private void checkPacketOrder(final @NotNull ExpectVehiclePacket current) {
-    checkState(expectVehiclePacket == current, expectVehiclePacket == null
-      ? "sending vehicle packet while not in vehicle (" + current.formattedName() + ")"
-      : "expected " + expectVehiclePacket.formattedName() + " but got " + current.formattedName());
   }
 }
