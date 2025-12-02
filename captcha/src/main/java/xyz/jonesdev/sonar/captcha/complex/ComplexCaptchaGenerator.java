41fe39cf0fae6b00e6c4b1e888c1f838d088f822
Jones <admin@jonesdev.xyz>
2025-12-02 21:35:24 +0100

    Fix incorrect file license header

--- captcha/src/main/java/xyz/jonesdev/sonar/captcha/complex/ComplexCaptchaGenerator.java
+++ captcha/src/main/java/xyz/jonesdev/sonar/captcha/complex/ComplexCaptchaGenerator.java
@@ -1,8 +1,18 @@
 /*
- * Copyright (C) 2025 Jones Development
+ * Copyright (C) 2025 Sonar Contributors
  *
- * All rights reserved.
- * This software is proprietary and cannot be copied, modified, or distributed without explicit permission.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package xyz.jonesdev.sonar.captcha.complex;
